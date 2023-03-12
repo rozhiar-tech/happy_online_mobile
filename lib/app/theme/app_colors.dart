df2339c5a915bcb32b660da7dc05f2f833d2d292
Rozhiar Omer <rozyar.313175000@uhd.edu.iq>
2023-03-12 21:08:38 +0300

    initial

---
+++ lib/app/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+
+class AppColors {
+  AppColors._();
+
+  static const Color darkPurple = Color(0xFF7F58AF);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
+// import 'package:flutter_native_splash/flutter_native_splash.dart';
+
 void main() {
   runApp(
     GetMaterialApp(
-      title: "Application",
+      debugShowCheckedModeBanner: false,
+      title: "Happy",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
